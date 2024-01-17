931e72054832bc4b664b28efc29fa4b88ad08a80
Recondit <61629691+Recondit@users.noreply.github.com>
2024-01-17 13:50:23 -0500

    Added two variants of lorentzattractor

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ struct Particle {
     sf::Vector3f position;
     sf::Color color;
     sf::VertexArray path;
+    sf::Vector3f Constant;
 };
 
 sf::Color randColor(float x , float y , float z){
@@ -28,16 +29,16 @@ sf::Color randColor(float x , float y , float z){
     return sf::Color(color.x , color.y , color.z);
 }
 
-void LorentzAttractor(){
+void SimpleLorentzAttractor(){
     // Create a window
     const int windowWidth = 1600;
     const int windowHeight = 900;
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Lorentz Attractor");
 
     // Create particles
-    const int numParticles = 1000; // Adjust the number of particles as needed
+    const int numParticles = 100; // Adjust the number of particles as needed
     std::vector<Particle> particles(numParticles);
-    const int remove = 60;
+    const int remove = 60; // Higher value means longer paths
 
     // Initialize particles with different initial positions and create their paths
     for (int i = 0; i < numParticles; ++i) {
@@ -46,6 +47,13 @@ void LorentzAttractor(){
         particles[i].position.z = static_cast<float>(rand() % 75 - 5);
 
         particles[i].color = randColor(particles[i].position.x , particles[i].position.y , particles[i].position.z);
+
+        float randomFactorx = (rand() % 141 - 70) / 100.0f;
+        float randomFactory = (rand() % 141 - 70) / 100.0f;
+        float randomFactorz = (rand() % 141 - 70) / 100.0f;
+        particles[i].Constant = sf::Vector3f(sigma * (1.0f+randomFactorx) , rho * (1.0f + randomFactory) , beta * (1.0f + randomFactorz));
+
+        printf("%f\n", particles[i].Constant.x);
 
         particles[i].path.setPrimitiveType(sf::LineStrip);
         particles[i].path.append(sf::Vertex(sf::Vector2f(windowWidth / 2 + particles[i].position.x * 18, windowHeight / 2 - particles[i].position.y * 18), particles[i].color));
@@ -117,55 +125,206 @@ void LorentzAttractor(){
 
 
 
-
-
-
-void DoublePendulum() {
-     // Create a window
+void LorentzAttractor(){
+    // Create a window
     const int windowWidth = 1600;
     const int windowHeight = 900;
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Lorentz Attractor");
-    // Create bob
-    Pendulum bob = {150.0f, 2.0f, 0.0f , 0.001f};
 
-   
+    // Create particles
+    const int numParticles = 200; // Adjust the number of particles as needed
+    std::vector<Particle> particles(numParticles);
+    const int remove = 60; // Higher value means longer paths
+
+    // Initialize particles with different initial positions and create their paths
+    for (int i = 0; i < numParticles; ++i) {
+        particles[i].position.x = static_cast<float>(rand() % 75 - 5);
+        particles[i].position.y = static_cast<float>(rand() % 75 - 5);
+        particles[i].position.z = static_cast<float>(rand() % 75 - 5);
+
+        particles[i].color = randColor(particles[i].position.x , particles[i].position.y , particles[i].position.z);
+
+        float randomFactorx = (rand() % 141 - 70) / 100.0f;
+        float randomFactory = (rand() % 141 - 70) / 100.0f;
+        float randomFactorz = (rand() % 141 - 70) / 100.0f;
+        particles[i].Constant = sf::Vector3f(sigma * (1.0f+randomFactorx) , rho * (1.0f + randomFactory) , beta * (1.0f + randomFactorz));
+
+        printf("%f\n", particles[i].Constant.x);
+
+        particles[i].path.setPrimitiveType(sf::LineStrip);
+        particles[i].path.append(sf::Vertex(sf::Vector2f(windowWidth / 2 + particles[i].position.x * 18, windowHeight / 2 - particles[i].position.y * 18), particles[i].color));
+    }
 
     // Main loop
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed){
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
         }
-        // Update
-        bob.angularVelocity += (-9.8 / bob.length * std::sin(bob.angle) - bob.damping * bob.angularVelocity) * 0.01f;
-        bob.angle += bob.angularVelocity * 0.01f;
-        printf("%f\n", bob.angle);
-        printf("%f\n", bob.angularVelocity);
 
+        // Update particles
+        for (int i = 0; i < numParticles; ++i) {
+            float x = particles[i].position.x;
+            float y = particles[i].position.y;
+            float z = particles[i].position.z;
 
+            float dx = particles[i].Constant.x  * (y - x);
+            float dy = x * (particles[i].Constant.y - z) - y;
+            float dz = x * y - particles[i].Constant.z * z;
 
+            // Update 
+            particles[i].position.x += dx * 0.001f;
+            particles[i].position.y += dy * 0.001f;
+            particles[i].position.z += dz * 0.001f;
 
-
-
+            // Append the current position to the path
+            particles[i].path.append(sf::Vertex(sf::Vector2f(windowWidth / 2 + particles[i].position.x * 18, windowHeight / 2 - particles[i].position.y * 18), particles[i].color));
+        }
 
         // Draw
         window.clear();
-        sf::Vector2f bobPosition = sf::Vector2f(bob.length * std::sin(bob.angle) , bob.length * std::cos(bob.angle));
-        sf::CircleShape ball(20.0f);
-        ball.setPosition(windowWidth/2 + bobPosition.x, windowHeight/2 + bobPosition.y);
-        ball.setFillColor(sf::Color::Red);
-        window.draw(ball);
+
+        // Draw the paths
+        for (int i = 0; i < numParticles; ++i) {
+            window.draw(particles[i].path);
+        }
+
+        // Draw the current positions
+        for (int i = 0; i < numParticles; ++i) {
+            sf::CircleShape circle(1.0f);
+            circle.setPosition(windowWidth / 2 + particles[i].position.x * 18, windowHeight / 2 - particles[i].position.y * 18);
+            circle.setFillColor(particles[i].color);
+            window.draw(circle);
+        }
+
+        for (int i = 0; i < numParticles; ++i) {
+            int r = rand() % 5;
+
+            if (particles[i].path.getVertexCount() > 2*remove){
+            sf::VertexArray copy(sf::LineStrip, particles[i].path.getVertexCount() - r);
+            for (int j = r; j < particles[i].path.getVertexCount(); ++j) {
+                        copy[j-r] = particles[i].path[j];
+            }
+            particles[i].path = copy;
+            }
+
+            
+
+
+            particles[i].path.append(sf::Vertex(sf::Vector2f(windowWidth / 2 + particles[i].position.x * 18, windowHeight / 2 - particles[i].position.y * 18), particles[i].color));
+        }
         window.display();
     }
+}
 
+
+// Constants
+const double g = 9.81;  // acceleration due to gravity
+
+// Function to update the state of the double pendulum using the Runge-Kutta method
+void update(double& theta1, double& theta2, double& omega1, double& omega2, double dt, double l1, double l2, double m1, double m2) {
+    // Runge-Kutta method
+    double k11, k12, k21, k22, k31, k32, k41, k42;
+    
+    k11 = dt * omega1;
+    k21 = dt * omega2;
+    
+    k12 = dt * (-g * (2 * m1 + m2) * std::sin(theta1) - m2 * g * std::sin(theta1 - 2 * theta2) -
+                2 * std::sin(theta1 - theta2) * m2 * (std::pow(omega2, 2) * l2 + std::pow(omega1, 2) * l1 * std::cos(theta1 - theta2))) /
+               (l1 * (2 * m1 + m2 - m2 * std::cos(2 * (theta1 - theta2))));
+    
+    k22 = dt * (2 * std::sin(theta1 - theta2) * (std::pow(omega1, 2) * l1 * (m1 + m2) + g * (m1 + m2) * std::cos(theta1) +
+                                                  std::pow(omega2, 2) * l2 * m2 * std::cos(theta1 - theta2))) /
+               (l2 * (2 * m1 + m2 - m2 * std::cos(2 * (theta1 - theta2))));
+    
+    k31 = dt * (omega1 + 0.5 * k12);
+    k41 = dt * (omega2 + 0.5 * k22);
+    
+    k32 = dt * (-g * (2 * m1 + m2) * std::sin(theta1 + 0.5 * k11) -
+                m2 * g * std::sin((theta1 + 0.5 * k11) - 2 * (theta2 + 0.5 * k21)) -
+                2 * std::sin((theta1 + 0.5 * k11) - (theta2 + 0.5 * k21)) * m2 *
+                    (std::pow((omega2 + 0.5 * k22), 2) * l2 + std::pow((omega1 + 0.5 * k12), 2) * l1 * std::cos((theta1 + 0.5 * k11) - (theta2 + 0.5 * k21)))) /
+               (l1 * (2 * m1 + m2 - m2 * std::cos(2 * ((theta1 + 0.5 * k11) - (theta2 + 0.5 * k21)))));
+    
+    k42 = dt * (2 * std::sin((theta1 + 0.5 * k11) - (theta2 + 0.5 * k21)) *
+                    (std::pow((omega1 + 0.5 * k12), 2) * l1 * (m1 + m2) +
+                     g * (m1 + m2) * std::cos((theta1 + 0.5 * k11)) +
+                     std::pow((omega2 + 0.5 * k22), 2) * l2 * m2 * std::cos((theta1 + 0.5 * k11) - (theta2 + 0.5 * k21)))) /
+               (l2 * (2 * m1 + m2 - m2 * std::cos(2 * ((theta1 + 0.5 * k11) - (theta2 + 0.5 * k21)))));
+    
+    theta1 += k31;
+    theta2 += k41;
+    omega1 += k32;
+    omega2 += k42;
+}
+
+
+
+
+
+void DoublePendulum() {
+    // Create a window
+    const int windowWidth = 800;
+    const int windowHeight = 600;
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Double Pendulum");
+
+    // Pendulum parameters
+    double theta1 = 3.14 / 2.0;  // initial angle of the first pendulum
+    double theta2 = 3.14 / 2.0;  // initial angle of the second pendulum
+    double omega1 = 0.0;         // initial angular velocity of the first pendulum
+    double omega2 = 0.0;         // initial angular velocity of the second pendulum
+    double l1 = 150.0;           // length of the first pendulum arm
+    double l2 = 150.0;           // length of the second pendulum arm
+    double m1 = 10.0;            // mass of the first bob
+    double m2 = 10.0;            // mass of the second bob
+
+    // Create a vertex array for the pendulum path
+    sf::VertexArray path1(sf::LineStrip);
+    sf::VertexArray path2(sf::LineStrip);
+
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Update pendulum state
+        update(theta1, theta2, omega1, omega2, 0.01, l1, l2, m1, m2);
+
+        // Append the current position to the paths
+        path1.append(sf::Vertex(sf::Vector2f(windowWidth / 2 + l1 * std::sin(theta1), windowHeight / 2 + l1 * std::cos(theta1)), sf::Color::Red));
+        path2.append(sf::Vertex(sf::Vector2f(windowWidth / 2 + l1 * std::sin(theta1) + l2 * std::sin(theta2), windowHeight / 2 + l1 * std::cos(theta1) + l2 * std::cos(theta2)), sf::Color::Green));
+
+        // Draw
+        window.clear();
+
+        // Draw the paths
+        window.draw(path1);
+        window.draw(path2);
+
+        // Draw the pendulum bobs
+        sf::CircleShape bob1(m1);
+        bob1.setPosition(windowWidth / 2 + l1 * std::sin(theta1) - m1, windowHeight / 2 + l1 * std::cos(theta1) - m1);
+        bob1.setFillColor(sf::Color::Red);
+        window.draw(bob1);
+
+        sf::CircleShape bob2(m2);
+        bob2.setPosition(windowWidth / 2 + l1 * std::sin(theta1) + l2 * std::sin(theta2) - m2, windowHeight / 2 + l1 * std::cos(theta1) + l2 * std::cos(theta2) - m2);
+        bob2.setFillColor(sf::Color::Green);
+        window.draw(bob2);
+
+        window.display();
+    }
 
 }
 
 int main() {
     // DoublePendulum();
     LorentzAttractor();
+    // SimpleLorentzAttractor();
     return 0;
 }
 
